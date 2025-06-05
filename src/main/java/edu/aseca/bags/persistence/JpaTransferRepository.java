05c99b10c47fe35ada8a895f450109122879572c
pablo <pbaratta@mail.austral.edu.ar>
2025-06-04 22:59:45 -0300

    fix: pagination fix

--- src/main/java/edu/aseca/bags/persistence/JpaTransferRepository.java
+++ src/main/java/edu/aseca/bags/persistence/JpaTransferRepository.java
@@ -1,11 +1,12 @@
 package edu.aseca.bags.persistence;
 
 import edu.aseca.bags.application.TransferRepository;
+import edu.aseca.bags.application.dto.PageResponse;
 import edu.aseca.bags.application.dto.Pagination;
 import edu.aseca.bags.domain.transaction.Transfer;
 import edu.aseca.bags.domain.transaction.TransferNumber;
 import edu.aseca.bags.domain.wallet.Wallet;
-import java.util.List;
+import edu.aseca.bags.service.PageResponseMapper;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Repository;
@@ -39,7 +40,7 @@ public class JpaTransferRepository implements TransferRepository {
 	}
 
 	@Override
-	public List<Transfer> findByFromWalletOrToWallet(Wallet fromWallet, Wallet toWallet, Pagination page) {
+	public PageResponse<Transfer> findByFromWalletOrToWallet(Wallet fromWallet, Wallet toWallet, Pagination page) {
 		WalletEntity fromWalletEntity = walletJpaRepository.findByEmail(fromWallet.getEmail().address())
 				.orElseThrow(() -> new IllegalStateException("From wallet not found"));
 		WalletEntity toWalletEntity = walletJpaRepository.findByEmail(toWallet.getEmail().address())
@@ -47,7 +48,9 @@ public class JpaTransferRepository implements TransferRepository {
 
 		Page<TransferEntity> pageResult = jpaRepository.findByFromWalletOrToWallet(fromWalletEntity, toWalletEntity,
 				org.springframework.data.domain.PageRequest.of(page.page(), page.size()));
-		return pageResult.stream().map(TransferMapper::toDomain).toList();
+		Page<Transfer> mappedPage = pageResult.map(TransferMapper::toDomain);
+
+		return PageResponseMapper.fromSpringPage(mappedPage);
 	}
 
 }
